b428417a7fe841733fda7f1d574c89776c50fc60
shaonahmedronok1 <shaonbtw@gmail.com>
2026-04-17 01:46:30 -0700

    what changed

--- modules/home/niri.nix
+++ modules/home/niri.nix
@@ -12,7 +12,7 @@ home.packages = with pkgs; [
     XDG_CURRENT_DESKTOP                 = "niri";
     XDG_SESSION_TYPE                    = "wayland";
     XDG_SESSION_DESKTOP                 = "niri";
-    QT_QPA_PLATFORM                     = "wayland";
+    QT_QPA_PLATFORM                     = "wayland;xcb";
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
     GDK_BACKEND                         = "wayland,x11";
   };
