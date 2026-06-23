d3372d96088fd3b7fe756f868afdb271c48bba3c
Shaon Ahmed Ronok <264064626+shaonahmedronok1@users.noreply.github.com>
2026-06-23 23:57:54 +0600

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "shaon's NixOS config";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.11";
+    nixpkgs.url = "nixpkgs/nixos-26.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix = {
-      url = "github:danth/stylix/release-25.11";
+      url = "github:danth/stylix/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
