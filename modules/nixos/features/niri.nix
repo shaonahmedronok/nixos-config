f15ff5d720e1c54c3193c37765b97281449b09bd
Shaon Ahmed Ronok <264064626+shaonahmedronok1@users.noreply.github.com>
2026-07-05 00:25:45 +0600

    Update niri.nix

--- modules/nixos/features/niri.nix
+++ modules/nixos/features/niri.nix
@@ -1,17 +1,15 @@
 { pkgs, ... }:
 {
   programs.niri.enable = true;
-
-  # Required for apps that link against GTK and need X11 fallback
-  # (KeepassXC, Inkscape, etc.)
   programs.xwayland.enable = true;
-
   xdg.portal = {
     enable       = true;
     extraPortals = [
-      pkgs.xdg-desktop-portal-gnome
       pkgs.xdg-desktop-portal-gtk
     ];
-    config.common.default = "*";
+    config.common = {
+      default = [ "gtk" ];
+      "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+    };
   };
 }
