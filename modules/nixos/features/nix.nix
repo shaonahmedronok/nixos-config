eb70f818d383ea6ad5cde2af4f4582dae38521b5
Shaon Ahmed Ronok <264064626+shaonahmedronok1@users.noreply.github.com>
2026-07-10 12:30:01 +0600

    Delete modules directory

--- modules/nixos/features/nix.nix
+++
@@ -1,8 +0,0 @@
-{ ... }:
-{
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.permittedInsecurePackages = [
-    "electron-37.10.3"
-  ];
-}
