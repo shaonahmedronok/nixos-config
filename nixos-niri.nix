40b620e5217e858fac3398fc5d48ea654de2cceb
Shaon Ahmed Ronok <264064626+shaonahmedronok1@users.noreply.github.com>
2026-07-11 07:27:56 +0600

    Update nixos-niri.nix

--- nixos-niri.nix
+++ nixos-niri.nix
@@ -6,10 +6,14 @@
     enable       = true;
     extraPortals = [
       pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-gnome
     ];
-    config.common = {
-      default = [ "gtk" ];
-      "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+    config.niri = {
+      default                                      = [ "gnome" "gtk" ];
+      "org.freedesktop.impl.portal.FileChooser"   = [ "gtk" ];
+      "org.freedesktop.impl.portal.ScreenCast"    = [ "gnome" ];
+      "org.freedesktop.impl.portal.Screenshot"    = [ "gnome" ];
+      "org.freedesktop.impl.portal.RemoteDesktop" = [ "gnome" ];
     };
   };
 }
