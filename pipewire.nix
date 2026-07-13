ae9ffff19ff7b658cd2f65c81e27de058727ffc7
Shaon Ahmed Ronok <264064626+shaonahmedronok1@users.noreply.github.com>
2026-07-13 11:53:39 +0600

    Update pipewire.nix

--- pipewire.nix
+++ pipewire.nix
@@ -8,4 +8,10 @@
     pulse.enable = true;
     jack.enable = true;
   };
+  services.pipewire.wireplumber.extraConfig."99-default-sink" = {
+    "monitor.alsa.rules" = [{
+      matches = [{ "node.name" = "alsa_output.pci-0000_00_1f.3.analog-stereo"; }];
+      actions = { update-props = { "priority.session" = 2000; }; };
+    }];
+  };
 }
