e78f5410647801da28861231f590a067c047a78f
Shaon Ahmed Ronok <264064626+shaonahmedronok@users.noreply.github.com>
2026-09-04 20:43:03 +0600

    updated

--- configuration.nix
+++ configuration.nix
@@ -355,11 +355,6 @@ in
           Mod+2 { focus-workspace 2; }
           Mod+3 { focus-workspace 3; }
           Mod+4 { focus-workspace 4; }
-          Mod+5 { focus-workspace 5; }
-          Mod+6 { focus-workspace 6; }
-          Mod+7 { focus-workspace 7; }
-          Mod+8 { focus-workspace 8; }
-          Mod+9 { focus-workspace 9; }
           Mod+Shift+1 { move-window-to-workspace 1; }
           Mod+Shift+2 { move-window-to-workspace 2; }
           Mod+Shift+3 { move-window-to-workspace 3; }
